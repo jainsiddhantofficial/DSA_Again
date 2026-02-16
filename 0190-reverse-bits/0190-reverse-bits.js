f37f996594b2c7305536c8e60d9b54741693667f
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2026-02-16 12:34:40 +0530

    Time: 65 ms (5.07%), Space: 50.5 MB (100%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,8 @@
+var reverseBits = function(n) {
+    let ans = 0;
+    for (let i = 1; i <= 32; i++) {
+        ans = (ans << 1) | (n & 1);
+        n >>>= 1;
+    }
+    return ans >>> 0; // To fix overflow.
+};
